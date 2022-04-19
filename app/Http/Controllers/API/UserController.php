0c618e44c247bebae95dfc79652dbfb18f7fd39d
alwijein <alwijein@gmail.com>
2022-04-20 04:20:44 +0800

    solve update profile with image

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -15,38 +15,39 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         try {
-          $request->validate(
-              [
-                  'name' => ['required'],
-                  'no_hp' => ['required', 'min:11', 'unique:users'],
-                  'nik_ktp' => ['required', 'unique:users'],
-                  'role' => ['required'],
-                  'password' => ['required'],
-              ],
-          );
+            $request->validate(
+                [
+                    'name' => ['required'],
+                    'no_hp' => ['required', 'min:11', 'unique:users'],
+                    'nik_ktp' => ['required', 'unique:users'],
+                    'role' => ['required'],
+                    'password' => ['required'],
+                ],
+            );
 
-          User::create([
-              'name' => $request->name,
-              'no_hp' => $request->no_hp,
-              'nik_ktp' => $request->nik_ktp,
-              'role' => $request->role,
-              'password' => Hash::make($request->password),
-          ]);
+            User::create([
+                'name' => $request->name,
+                'no_hp' => $request->no_hp,
+                'nik_ktp' => $request->nik_ktp,
+                'role' => $request->role,
+                'password' => Hash::make($request->password),
+            ]);
 
-          $user = User::where('no_hp', $request->no_hp)->first();
+            $user = User::where('no_hp', $request->no_hp)->first();
 
-          $tokenResult = $user->createToken('authToken')->plainTextToken;
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
 
-          return ResponseFormatter::success(['access_token' => $tokenResult, 'token_type' => 'Bearer', $user], 'Data berhasil dibuat');
-
+            return ResponseFormatter::success(['access_token' => $tokenResult, 'token_type' => 'Bearer', $user], 'Data berhasil dibuat');
         } catch (\Exception $error) {
-            return ResponseFormatter::error(['message' => 'something went wrong' , 'error' => $error], "Data gagal ditambahkan", '500');
+            return ResponseFormatter::error(['message' => 'something went wrong', 'error' => $error], "Data gagal ditambahkan", '500');
         }
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         try {
             $request->validate(
                 [
@@ -60,7 +61,7 @@ class UserController extends Controller
                 'password'
             ]);
 
-            if(!Auth::attempt($credentials)){
+            if (!Auth::attempt($credentials)) {
                 return ResponseFormatter::error([
                     'massage' => 'Unauthorized'
                 ], 'Authentication Failed', 500);
@@ -68,7 +69,7 @@ class UserController extends Controller
 
             $user = User::where('no_hp', $request->no_hp)->first();
 
-            if(!Hash::check($request->password, $user->password, [])){
+            if (!Hash::check($request->password, $user->password, [])) {
                 throw new \Exception('Invalid Credentials');
             }
 
@@ -87,54 +88,69 @@ class UserController extends Controller
         }
     }
 
-    public function fetch(Request $request){
+    public function fetch(Request $request)
+    {
         return ResponseFormatter::success($request->user(), 'Data user berhasil diambil');
     }
 
-    public function updateProfile(Request $request){
-        try {
+    public function updateProfile(Request $request)
+    {
+        // try {
             $request->validate([
                 'name' => ['required'],
-                'no_hp' => ['required', 'string', 'max:255', 'unique:users,no_hp,'.$request->user()->id],
+                'no_hp' => ['required', 'string', 'max:255', 'unique:users,no_hp,' . $request->user()->id],
                 'alamat' => ['string'],
             ]);
 
-            $data = $request->all();
+            if ($request->hasFile('avatar')) {
+                $image = $request->file('avatar');
 
-            $user = Auth::user();
-            $user->update($data);
+                $image_name = time() . '.' . $image->getClientOriginalExtension();
+                // $destinationPath = base_path('images/users');
+                $image->storeAs("images/profile", $image_name);
+            } else {
+                dd('Request Has No File');
+            }
+
+            $user = User::where('id', Auth::user()->id)->update([
+                'name' => $request->name,
+                'no_hp' => $request->no_hp,
+                'alamat' => $request->alamat,
+                'avatar' => $image_name,
+            ]);
 
             return ResponseFormatter::success($user, 'Profil Updated');
-
-        } catch (Exception $error) {
-            return ResponseFormatter::error([
-                'massage' => 'Something went wrong',
-                'error' => $error,
-            ], 'Updated Failed', 500);
-        }
+        // } catch (Exception $error) {
+        //     return ResponseFormatter::error([
+        //         'massage' => 'Something went wrong',
+        //         'error' => $error,
+        //     ], 'Updated Failed', 500);
+        // }
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         $token = $request->user()->currentAccessToken()->delete();
 
         return ResponseFormatter::success($token, 'Token Revoked');
     }
 
-    public function getAll(Request $request){
+    public function getAll(Request $request)
+    {
         $id = $request->input('id');
         $role = $request->input('role');
         $limit = $request->input('limit');
 
-        if($id){
+        if ($id) {
             $user = User::find($id)->first();
-            if($user){
+            if ($user) {
                 return ResponseFormatter::success($user, 'Data User Berhasil Diambil');
-            }else{
+            } else {
                 return ResponseFormatter::error(null, 'Data User Tidak Ada', 404);
             }
         }
 
-        if ($role){
+        if ($role) {
             $user = User::where('role', $role)->get();
         }
 
@@ -142,8 +158,5 @@ class UserController extends Controller
             $user,
             'Data berhasil diambil'
         );
-
-
-
     }
 }
